18bd130f32b7a4b62507605d67606c8f53db2a61
Albert <albert11al@example.com>
2022-06-18 05:09:11 +0300

    lesson_7 Logging

--- config/settings.py
+++ config/settings.py
@@ -160,3 +160,29 @@ SOCIAL_AUTH_GITHUB_KEY = "ef138a184a0a21ec5430"
 SOCIAL_AUTH_GITHUB_SECRET = "2eec63f7a8d403c9490cf6738c1bcef044dacf5f"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOG_FILE = BASE_DIR / "var" / "log" / "main_log.log"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console": {"format": "[%(asctime)s] %(levelname)s %(name)s (%(lineno)d) %(message)s"},
+    },
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": LOG_FILE,
+            "formatter": "console",
+        },
+        "console": {"class": "logging.StreamHandler", "formatter": "console"},
+    },
+    "loggers": {
+        "django": {"level": "INFO", "handlers": ["console"]},
+        "mainapp": {
+            "level": "DEBUG",
+            "handlers": ["file"],
+        },
+    },
+}
